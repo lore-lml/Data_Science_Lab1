7e18b2677b707276af652be6774d291b88ae1d28
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2019-10-23 20:03:51 +0200

    2.2

--- script1.py
+++ script1.py
@@ -1,79 +1,51 @@
 import csv
-from math import sqrt, pow
+from math import sqrt
 
-
-def avg(sum, len):
-    return float(sum) / len
+def mean(sum, length):
+    return float(sum) / length
 
 
 def quad_dev(val, avg):
-    return pow(val - avg, 2)
+    return (val - avg) ** 2
 
 
-def std_dev(sum_quad_dev, len):
-    return sqrt(sum_quad_dev / len)
+def std_dev(sum_quad_dev, length):
+    return sqrt(sum_quad_dev / length)
 
-dataset = list()
-with open("data_sets/iris.csv") as iris:
-    for row in csv.reader(iris):
-        row = list(row)
-        if len(row) != 0:
-            dataset.append(row)
 
-# Calcolo media per ogni colonna e diferenziazione dei tre dataset
-sl = sw = pl = pw = .0
-categories = {}
-for row in dataset:
-    sl += float(row[0])
-    sw += float(row[1])
-    pl += float(row[2])
-    pw += float(row[3])
+if __name__ == '__main__':
+    dataset = list()
 
-    category = row[4].lower()
-    if category not in categories:
-        categories[category] = []
-    else:
-        categories[category].append(row)
+    with open("data_sets/iris.csv") as iris:
+        for row in csv.reader(iris):
+            row = list(row)
+            if len(row) != 0:
+                dataset.append(row)
 
-n_rows = len(dataset)
-sl = avg(sl, n_rows)
-sw = avg(sw, n_rows)
-pl = avg(pl, n_rows)
-pw = avg(pw, n_rows)
-
-# Calcolo std dev per ogni colonna
-std_dev_sl = std_dev_sw = std_dev_pl = std_dev_pw = 0
-for row in dataset:
-    std_dev_sl += quad_dev(float(row[0]), sl)
-    std_dev_sw += quad_dev(float(row[1]), sw)
-    std_dev_pl += quad_dev(float(row[2]), pl)
-    std_dev_pw += quad_dev(float(row[3]), pw)
-
-std_dev_sl = std_dev(std_dev_sl, n_rows)
-std_dev_sw = std_dev(std_dev_sw, n_rows)
-std_dev_pl = std_dev(std_dev_pl, n_rows)
-std_dev_pw = std_dev(std_dev_pw, n_rows)
-
-print(f"Avg: [{sl:.2f}, {sw:.2f}, {pl:.2f}, {pw:.2f}]")
-print(f"Std Dev: [{std_dev_sl:.2f}, {std_dev_sw:.2f}, {std_dev_pl:.2f}, {std_dev_pw:.2f}]")
-
-# Calcolo media per ogni categoria
-for category in categories.values():
+    # Calcolo media per ogni colonna e diferenziazione dei tre dataset
     sl = sw = pl = pw = .0
-    for row in category:
+    categories = {}
+    for row in dataset:
         sl += float(row[0])
         sw += float(row[1])
         pl += float(row[2])
         pw += float(row[3])
 
-    n_rows = len(category)
-    sl = avg(sl, n_rows)
-    sw = avg(sw, n_rows)
-    pl = avg(pl, n_rows)
-    pw = avg(pw, n_rows)
+        category = row[4].lower()
+        if category not in categories:
+            categories[category] = []
+        else:
+            categories[category].append(row)
 
+    n_rows = len(dataset)
+    sl = mean(sl, n_rows)
+    sw = mean(sw, n_rows)
+    pl = mean(pl, n_rows)
+    pw = mean(pw, n_rows)
+
+    # Calcolo std dev per ogni colonna
     std_dev_sl = std_dev_sw = std_dev_pl = std_dev_pw = 0
-    for row in category:
+    for row in dataset:
         std_dev_sl += quad_dev(float(row[0]), sl)
         std_dev_sw += quad_dev(float(row[1]), sw)
         std_dev_pl += quad_dev(float(row[2]), pl)
@@ -84,6 +56,36 @@ for category in categories.values():
     std_dev_pl = std_dev(std_dev_pl, n_rows)
     std_dev_pw = std_dev(std_dev_pw, n_rows)
 
-    print(f"\n{category[0][4]}:")
     print(f"Avg: [{sl:.2f}, {sw:.2f}, {pl:.2f}, {pw:.2f}]")
     print(f"Std Dev: [{std_dev_sl:.2f}, {std_dev_sw:.2f}, {std_dev_pl:.2f}, {std_dev_pw:.2f}]")
+
+    # Calcolo media per ogni categoria
+    for category in categories.values():
+        sl = sw = pl = pw = .0
+        for row in category:
+            sl += float(row[0])
+            sw += float(row[1])
+            pl += float(row[2])
+            pw += float(row[3])
+
+        n_rows = len(category)
+        sl = mean(sl, n_rows)
+        sw = mean(sw, n_rows)
+        pl = mean(pl, n_rows)
+        pw = mean(pw, n_rows)
+
+        std_dev_sl = std_dev_sw = std_dev_pl = std_dev_pw = 0
+        for row in category:
+            std_dev_sl += quad_dev(float(row[0]), sl)
+            std_dev_sw += quad_dev(float(row[1]), sw)
+            std_dev_pl += quad_dev(float(row[2]), pl)
+            std_dev_pw += quad_dev(float(row[3]), pw)
+
+        std_dev_sl = std_dev(std_dev_sl, n_rows)
+        std_dev_sw = std_dev(std_dev_sw, n_rows)
+        std_dev_pl = std_dev(std_dev_pl, n_rows)
+        std_dev_pw = std_dev(std_dev_pw, n_rows)
+
+        print(f"\n{category[0][4]}:")
+        print(f"Avg: [{sl:.2f}, {sw:.2f}, {pl:.2f}, {pw:.2f}]")
+        print(f"Std Dev: [{std_dev_sl:.2f}, {std_dev_sw:.2f}, {std_dev_pl:.2f}, {std_dev_pw:.2f}]")
--- script2.py
+++ script2.py
@@ -1,21 +1,54 @@
-import json
+import json, sys
+from math import cos, acos, sin
+from random import randint
 
-with open("data_sets/to-bike.json") as dataset:
-    js = json.load(dataset)
 
-# Calcolo tutte le stazioni online
+def distance_coords(lat1, lng1, lat2, lng2):
+    """Compute the distance among two points."""
+    deg2rad = lambda x: x * 3.141592 / 180
+    lat1, lng1, lat2, lng2 = map(deg2rad, [ lat1, lng1, lat2, lng2 ])
+    R = 6378100 # Radius of the Earth, in meters
+    return R * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lng1 - lng2))
 
-# Calcolo tutte le free bikes e gli empty slot
-stations = js['network']['stations']
 
-online_stations = free_bikes = empty_slots = 0
-for station in stations:
-    if station['extra']['status'].lower() == "online":
-        online_stations += 1
+if __name__ == '__main__':
+    with open("data_sets/to-bike.json") as dataset:
+        js = json.load(dataset)
 
-    free_bikes += station['free_bikes']
-    empty_slots += station['empty_slots']
+    # Calcolo tutte le stazioni online
 
-print(f"online stations: {online_stations}")
-print(f"free bikes: {free_bikes}")
-print(f"empty slots: {empty_slots}")
+    # Calcolo tutte le free bikes e gli empty slot
+    stations = js['network']['stations']
+
+    online_stations = []
+    free_bikes = empty_slots = 0
+    for station in stations:
+        if station['extra']['status'].lower() == "online":
+            online_stations.append(station)
+            free_bikes += station['free_bikes']
+            empty_slots += station['empty_slots']
+
+    print(f"online stations: {len(online_stations)}")
+    print(f"free bikes: {free_bikes}")
+    print(f"empty slots: {empty_slots}")
+
+    chosen_index = randint(0, len(online_stations))
+    fixed_latitude = stations[chosen_index]['latitude']
+    fixed_longitude = stations[chosen_index]['longitude']
+    distance = sys.maxsize
+    closest_station = -1
+
+    for station in stations:
+        if stations[chosen_index] == station:
+            continue
+        current_latitude = station['latitude']
+        current_longitude = station['longitude']
+        current_distance = distance_coords(fixed_latitude, fixed_longitude, current_latitude, current_longitude)
+        if current_distance < distance:
+            closest_station = station
+            distance = current_distance
+
+    print(f"\nChoosen Station: {stations[chosen_index]['name']}")
+    print(f"Closest Station: {closest_station['name']}, {distance:.2f}m")
+
+
